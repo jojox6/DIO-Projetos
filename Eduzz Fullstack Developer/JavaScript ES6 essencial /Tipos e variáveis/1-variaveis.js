b03b60299f374321ccdd6c867f65db49ff60f722
jojox6 <dougx6@gmail.com>
2022-01-02 12:25:02 -0300

    Variaveis em JavaScript

---
+++ Eduzz Fullstack Developer/JavaScript ES6 essencial /Tipos e variáveis/1-variaveis.js
@@ -0,0 +1,7 @@
+var nameVar = 'Joseph';
+let nameLet = 'Joseph';
+const nameConst = 'Joseph';
+
+console.log(`nameVar: ${nameVar}`);
+console.log(`nameLet: ${nameLet}`);
+console.log(`nameConst: ${nameConst}`);
